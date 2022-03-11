a23ef298ef668891e542179986d0738b0910d7a7
Ed Markovich <em1258@stern.nyu.edu>
2022-03-11 09:09:05 -0500

    Update day19.py

--- day19.py
+++ day19.py
@@ -11,6 +11,7 @@ class Scanner:
         self.beacons     = beacons
         self.distances   = None
         self.computeDistances()
+        self.relocated   = False
 
     def distance(self, a, b):        
         x = (a[0]-b[0]) ** 2
@@ -34,17 +35,18 @@ class Scanner:
         
         return self.distances
 
-    def countOverlap(self, otherSensor):
-        x = self.computeDistances()        
+    def getOverlaps(self, otherSensor):
+        x = self.computeDistances()   
+        out =[]     
         for beacon in x.keys():
             dists = x[beacon].values()
-            matchedBeacon = otherSensor.findOverlap(dists)  
+            matchedBeacon = otherSensor.findOverlapByDistances(dists)  
             if matchedBeacon != None:
-                print(self.beacons[beacon], " -> ", matchedBeacon)
+                out.append((self.beacons[beacon], matchedBeacon))                
 
-        return False
+        return out
             
-    def findOverlap(self, otherDistances):
+    def findOverlapByDistances(self, otherDistances):
         myDs = self.computeDistances()
         for myB in myDs.keys():
             overlap = set(myDs[myB].values()).intersection(set(otherDistances))
@@ -78,11 +80,100 @@ def loadScanners(Lines):
     
     return scanners
 
-scans = loadScanners(Lines)
+def getDistVect(a,b):
+    return( a[0]-b[0], a[1]-b[1], a[2]-b[2] )
 
-for i in range(0, len(scans)):
-    for j in range(i+1, len(scans)):
-        print(i,j)
-        x = scans[i].countOverlap(scans[j])
+def getTranslator(colSeq, colSign):
+    funcA = None
+    if colSeq == 1: funcA = lambda x : (x[0], x[1], x[2])
+    if colSeq == 2: funcA = lambda x : (x[0], x[2], x[1])
+    if colSeq == 3: funcA = lambda x : (x[1], x[0], x[2])
+    if colSeq == 4: funcA = lambda x : (x[1], x[2], x[0])
+    if colSeq == 5: funcA = lambda x : (x[2], x[0], x[1])
+    if colSeq == 6: funcA = lambda x : (x[2], x[1], x[0])
+
+    if colSign == 1: funcB = lambda x : (x[0], x[1], x[2])
+    if colSign == 2: funcB = lambda x : (-x[0], x[1], x[2])
+    if colSign == 3: funcB = lambda x : (x[0], -x[1], x[2])
+    if colSign == 4: funcB = lambda x : (x[0], x[1], -x[2])
+    if colSign == 5: funcB = lambda x : (-x[0], -x[1], x[2])
+    if colSign == 6: funcB = lambda x : (x[0], -x[1], -x[2])    
+    if colSign == 7: funcB = lambda x : (-x[0], -x[1], -x[2]) 
+
+    g = lambda x: funcB(funcA(x))
+    return g
+
+def findFindAxisFunction(overlappedBeacons):
+    for a in range(1,6):
+        for b in range (1, 8):
+            axisFunction = getTranslator(a,b)
+
+            test = list(map(lambda x: (x[0], axisFunction(x[1])), overlappedBeacons))
+            test = list(filter(lambda x: getDistVect(x[0], x[1]), test))
+            print("    :", test)
+            if len(test) == len(overlappedBeacons):
+                return axisFunction
+    print("No Axis rotator?!")
+
+
+def findMoveVector(overlappedBeacons):
+
+    for i in range(0, len(overlappedBeacons)-1):
+        x1 = getDistVect(overlappedBeacons[i][0], overlappedBeacons[i+1][0])
+        x2 = getDistVect(overlappedBeacons[i][1], overlappedBeacons[i+1][1])
+        if x1 == x2 : 
+            offset = getDistVect(overlappedBeacons[i][1], overlappedBeacons[i][0])
+            print("OOOOOH",overlappedBeacons[i][0], overlappedBeacons[i][1], offset)
+            return offset
+        print("WTF?!",x1,x2)
+    print("ERROR?!")
+
+
+def getScanPairsForMatching(scans):
+    relocated = []
+    todo      = []
+
+    for s in scans:
+        if s.relocated:  relocated.append(s)
+        else:            todo.append(s)
+
+    if todo == []: return None
+
+    out = []
+    for r in relocated:
+        for t in todo:
+            out.append( (r,t) )
+
+    return out
+
+
+scans = loadScanners(Lines)
+scans[0].relocated = True
+
+while True:
+    pairs = getScanPairsForMatching(scans)
+    if pairs == None: break
+
+    for p in pairs:
+        overlaps = p[0].getOverlaps(p[1])
+        if overlaps == []: 
+            continue
+
+        print(overlaps[0])
+
+        p[1].relocated = True
+        axisFunction = findFindAxisFunction(overlaps)
+
+        overlapWithRotation = list(map(lambda x: (x[0], axisFunction(x[1])), overlaps))
+        print(overlapWithRotation[0])
+
+        sys.exit()
+
+        findMoveVector(overlapWithRotation)
         
+
+
+
+
+
         
